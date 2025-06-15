d8a665266267bfd44a1b6c706498358f88e8c3ed
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-06-15 20:04:41 +0900

    DISCORD_BOT_TOKEN..TOKEN_DISCORD

--- youtube-status-bot.py
+++ youtube-status-bot.py
@@ -42,8 +42,8 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN_DISCORD=os.environ['DISCORD_BOT_TOKEN']
 TOKEN_YOUTUBE=os.environ['YOUTUBE_ACCESS_TOKEN']
+TOKEN_DISCORD=os.environ['TOKEN_DISCORD']
 
 
 client = None
