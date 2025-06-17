42ae76ef167f504ac8b41c1331702f750abb0982
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-06-17 18:02:43 +0900

    on_message_edit

--- youtube-status-bot.py
+++ youtube-status-bot.py
@@ -213,7 +213,7 @@ async def on_message_edit(before, after):
         after.channel.name,
         after.content,
     ))
-    await on_message(after)
+    # await on_message(after)
 
 @client.event
 async def on_message_delete(message):
