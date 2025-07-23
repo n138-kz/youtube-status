0dc8a735a535d590a5015bf5e4d6ffd321d286dc
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-23 21:47:58 +0900

    psycopg2.errors.DatatypeMismatch

--- youtube-status-bot.py
+++ youtube-status-bot.py
@@ -248,7 +248,7 @@ def store_c_info(dsn='', data={}):
                 ))
                 conn.commit()
                 return True
-    except (Exception) as error:
+    except (Exception, psycopg2.errors.DatatypeMismatch) as error:
         logger.error(f'Error has occured in Database operation: {error}')
         logger.error(f'{sys.exc_info()}')
         logger.error(f'{traceback.format_exc()}')
